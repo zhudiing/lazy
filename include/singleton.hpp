94712d891bfc412bd3ed0504bfffa847c6cc0f96
朱迪 <45189619+zhudiing@users.noreply.github.com>
2023-03-27 11:56:36 +0800

    Create singleton.hpp

---
+++ include/singleton.hpp
@@ -0,0 +1,64 @@
+/** 
+ * @Date         : 2023-03-23 15:17:27
+ * @LastEditors  : zhudi
+ * @LastEditTime : 2023-03-24 17:42:56
+ */
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cerrno>
+#include <string>
+#include <cstring>
+#include <stdexcept>
+
+using namespace std;
+class SingletonProcess
+{
+public:
+    SingletonProcess(uint16_t port0)
+            : socket_fd(-1)
+              , rc(1)
+              , port(port0)
+    {
+    }
+
+    ~SingletonProcess()
+    {
+        if (socket_fd != -1)
+        {
+            close(socket_fd);
+        }
+    }
+
+    bool operator()()
+    {
+        if (socket_fd == -1 || rc)
+        {
+            socket_fd = -1;
+            rc = 1;
+
+            if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+            {
+                throw std::runtime_error(std::string("Could not create socket: ") +  strerror(errno));
+            }
+            else
+            {
+                struct sockaddr_in name;
+                name.sin_family = AF_INET;
+                name.sin_port = htons (port);
+                name.sin_addr.s_addr = htonl (INADDR_ANY);
+                rc = bind (socket_fd, (struct sockaddr *) &name, sizeof (name));
+            }
+        }
+        return (socket_fd != -1 && rc == 0);
+    }
+
+    std::string GetLockFileName()
+    {
+        return "port " + std::to_string(port);
+    }
+
+private:
+    int socket_fd = -1;
+    int rc;
+    uint16_t port;
+};
